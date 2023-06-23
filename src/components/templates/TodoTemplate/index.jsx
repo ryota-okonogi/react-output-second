7dd8acedd3aa67eb5c29660af8f2c307f0a2ab9b
nogi5000 <R5000.power@gmail.com>
2023-06-23 23:38:43 +0900

    add: TodoTemplate2-2

---
+++ src/components/templates/TodoTemplate/index.jsx
@@ -0,0 +1,15 @@
+import { INIT_TODO_LIST } from "../../../constants/data"
+
+export const TodoTemplate = () => {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      {/* <INIT_TODO_LIST /> */}
+    </div>
+  )
+}
+
+/**
+ * @表示
+ * h1 = OK, <INIT_TODO_LIST /> = NG
+ */

c38a6ba0f3520142df5d1f73338e000552bba5be
nogi5000 <R5000.power@gmail.com>
2023-06-27 20:48:55 +0900

    検索フォームに入力した文字を前方一致検索した結果が更新後の画面に描画されない問題の原因を解決

--- src/utils/todoLogic.ts
+++ src/utils/todoLogic.ts
@@ -11,9 +11,8 @@
  * @returns
  */
 
-export const searchTodo = (todoList, keyword) => {
+export const searchTodo = (todoList, keyword) =>
   todoList.filter((todo) => { // propsのtodoListからtodo配列を生成し、そこから条件を付けてfilterする
     const regexp = new RegExp("^" + keyword, "i"); // Regexpメソッドの引数に前方一致検索の条件を渡す
     return todo.title.match(regexp); // todoのタイトルが前方一致検索の条件と一致するか照合する
   });
-}
